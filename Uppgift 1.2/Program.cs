b5dddc5c66bae5e7045ecfe65c45882535e22c8a
thbr06001 <thbr06001@E-PW0234MD>
2023-09-06 10:35:32 +0200

    Add project files.

---
+++ Uppgift 1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White; //Text färg vit
+            Console.BackgroundColor = ConsoleColor.DarkBlue; //Bakgrundfärg blå i terminalen
+            Console.Clear(); //Gör så att bara min bakgrundsfärg gäller
+            Console.WriteLine("!!WARNING!! VIRUS DETECTED !!WARNING!!"); //Lite text 
+            Console.ReadKey(); 
+        }
+    }
+}
